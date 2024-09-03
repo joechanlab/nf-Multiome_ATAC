d966c7e23725a9f7550f06745a4af1497d6f9725
Mamie Wang <wangm10@mskcc.org>
2024-09-03 16:09:21 -0400

    Corrected input arguments to peak2gene

--- bin/atac_Peak2Genes.R
+++ bin/atac_Peak2Genes.R
@@ -5,6 +5,7 @@ suppressMessages({
     library(parallel)
     library(stringr)
     library(Matrix)
+    library(anndata)
     library(optparse)
 })
 
@@ -16,39 +17,49 @@ addArchRGenome('hg38')
 
 # Parse command line arguments
 option_list <- list(
-    make_option(c("-o", "--out_dir"), type="character", default=NULL,
-                help="Output directory", metavar="PATH"),
-    make_option(c("-m", "--mtx_file"), type="character", default=NULL,
-                help="Path to counts matrix file", metavar="FILE"),
-    make_option(c("-f", "--feature_matrix"), type="character", default=NULL,
+    make_option(c("-s", "--sample"), type="character", default=NULL,
+                help="Sample name", metavar="CHARACTER"),
+    make_option(c("-a", "--atac_dir"), type="character", default=NULL,
+                help="Directory containing Arrow files", metavar="PATH"),
+    make_option(c("-r", "--rna_h5"), type="character", default=NULL,
                 help="Path to 10x feature matrix h5 file", metavar="FILE"),
-    make_option(c("-a", "--arrow_dir"), type="character", default=NULL,
-                help="Directory containing Arrow files", metavar="PATH")
+    make_option(c("-d", "--rna_h5ad"), type="character", default=NULL,
+                help="Path to 10x feature matrix h5 file", metavar="FILE"),
+    make_option(c("-o", "--out_dir"), type="character", default=NULL,
+                help="Output directory", metavar="PATH")
 )
 
 opt_parser <- OptionParser(option_list=option_list)
 opt <- parse_args(opt_parser)
 
 # Validate required arguments
-if (is.null(opt$out_dir) || is.null(opt$mtx_file) || is.null(opt$feature_matrix) || is.null(opt$arrow_dir)) {
+if (is.null(opt$atac_dir) || is.null(opt$rna_h5) || is.null(opt$rna_h5ad)) {
     print_help(opt_parser)
     stop("All arguments are required.", call.=FALSE)
 }
 
-setwd(opt$out_dir)
+proj <- loadArchRProject(path = opt$atac_dir)
 
-proj <- loadArchRProject(path = opt$out_dir)
+# Load processed scRNAs-seq h5ad
+rna_h5ad <- read_h5ad(opt$rna_h5ad)
+
+# Check if cell names start with opt$sample followed by #
+if (!all(startsWith(rna_h5ad$obs_names, paste0(opt$sample, "#")))) {
+    # Prepend the prefix only to cell names that don't have it
+    rna_h5ad$obs_names <- ifelse(
+        startsWith(rna_h5ad$obs_names, paste0(opt$sample, "#")),
+        rna_h5ad$obs_names,
+        paste0(opt$sample, "#", rna_h5ad$obs_names)
+    )
+}
 
 # Gene Activity
-counts <- t(as.matrix(read.table(opt$mtx_file, header=T, row.names=1, sep=',', comment.char = "!")))
-
 tmp <- import10xFeatureMatrix(
-    input = c(opt$feature_matrix),
-    names = c("RU1042_PDX")
+    input = c(opt$rna_h5),
+    names = c(opt$sample)
 )
-
 feature_df <- rowData(tmp)
-g2 <- intersect(rownames(feature_df), rownames(counts))
+g2 <- intersect(rownames(feature_df), colnames(rna_h5ad))
 feature_df <- feature_df[g2,]
 
 tmp <- str_split(feature_df$interval,'[:-]')
@@ -57,9 +68,9 @@ colnames(tmp2) <- c('chr','start','stop')
 rowRanges <- makeGRangesFromDataFrame(tmp2)
 
 # Create single cell experiment
-seRNA <- SummarizedExperiment(list(counts=counts[g2,]), rowRanges=rowRanges)
+seRNA <- SummarizedExperiment(list(counts=t(rna_h5ad[,g2]$X)), rowRanges=rowRanges)
 
-inputFiles <- list.files(opt$arrow_dir, full.names=T)
+inputFiles <- list.files(file.path(opt$atac_dir, 'ArrowFiles'), full.names=T)
 names(inputFiles) <- gsub('.arrow','',basename(inputFiles))
 
 # Add scRNA
@@ -80,11 +91,11 @@ p2g <- getPeak2GeneLinks(
 
 p2g$geneName <- mcols(metadata(p2g)$geneSet)$name[p2g$idxRNA]
 p2g$peakName <- (metadata(p2g)$peakSet %>% {paste0(seqnames(.), "_", start(.), "_", end(.))})[p2g$idxATAC]
-
 p2g <- as.data.frame(p2g)
 
 # Save
-proj <- saveArchRProject(ArchRProj = proj)
+dir.create(opt$out_dir, showWarnings = FALSE, recursive = TRUE)
+proj <- saveArchRProject(ArchRProj = proj, outputDirectory = opt$out_dir)
 
 # Gene scores
-write.csv(p2g, file.path(opt$out_dir, 'export/peak2genes.csv'), quote=FALSE)
+write.csv(p2g, file.path(opt$out_dir, 'peak2genes.csv'), quote=FALSE)
---
+++ modules/atac_peak2gene/main.nf
@@ -0,0 +1,27 @@
+process ATAC_PEAK2GENE {
+    label 'process_medium'
+    conda '/usersoftware/chanj3/ArchR'
+    publishDir "${params.outdir}/atac_peak2gene/", mode: 'copy'
+
+    input:
+    val name
+    path atac_dir
+    val rna_h5
+    val rna_h5ad
+
+    output:
+    val name, emit: name
+    path "${name}_peak2gene", emit: out_dir
+    val rna_h5, emit: rna_h5
+    val rna_h5ad, emit: rna_h5ad
+
+    script:
+    """
+    Rscript ${baseDir}/bin/atac_Peak2Genes.R \
+        -s ${name} \
+        -a ${atac_dir} \
+        -r ${rna_h5} \
+        -d ${rna_h5ad} \
+        -o ${name}_peak2gene
+    """
+}
